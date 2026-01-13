a179edd9640e918bdcc879312610ce896da44737
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-12 22:20:06 -0500

    Create Plugin class with boot method and hooks

---
+++ src/Core/Plugin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AperturePro\Core;
+
+use AperturePro\Http\Rest\ProjectStatusController;
+use AperturePro\Http\Rest\ProofingController;
+use AperturePro\Http\Rest\EditingController;
+use AperturePro\Http\Rest\DeliveryController;
+use AperturePro\Domain\Jobs\JobRunner;
+
+class Plugin
+{
+    public function boot(): void
+    {
+        Hooks::register();
+
+        add_action('rest_api_init', function () {
+            (new ProjectStatusController())->register_routes();
+            (new ProofingController())->register_routes();
+            (new EditingController())->register_routes();
+            (new DeliveryController())->register_routes();
+        });
+
+        add_action('ap_run_job', [JobRunner::class, 'handle'], 10, 1);
+    }
+}
