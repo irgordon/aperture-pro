f0701c40839286d365a2e0b5a15bc8086500b754
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-12 23:29:37 -0500

    Create StorageManager.php

---
+++ src/Storage/StorageManager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AperturePro\Storage;
+
+use AperturePro\Support\Error;
+
+class StorageManager
+{
+    protected static ?StorageAdapterInterface $adapter = null;
+
+    public static function boot(): void
+    {
+        $key = StorageSettings::getAdapterKey();
+
+        switch ($key) {
+            case 'local':
+                self::$adapter = new LocalStorageAdapter();
+                break;
+
+            default:
+                Error::logAndThrow('Unknown storage adapter: ' . $key);
+        }
+    }
+
+    public static function adapter(): StorageAdapterInterface
+    {
+        if (!self::$adapter) {
+            self::boot();
+        }
+        return self::$adapter;
+    }
+}
