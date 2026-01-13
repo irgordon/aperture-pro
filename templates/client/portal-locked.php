f6b0aa8817e7200591ae6d92c165210c95c569bf
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-12 23:10:20 -0500

    Create portal-locked.php

---
+++ templates/client/portal-locked.php
@@ -0,0 +1,4 @@
+<section class="ap-client-section ap-client-locked">
+    <h2>We’re working on your photos</h2>
+    <p>Your gallery isn’t ready to view just yet. You’ll receive an email as soon as it’s available.</p>
+</section>
