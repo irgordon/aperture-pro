0953a35b988ba12e2410b866c2ea36f29736e5bb
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-12 22:47:38 -0500

    Create ProofingEvents class with event constants

---
+++ src/Domain/Proofing/ProofingEvents.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AperturePro\Domain\Proofing;
+
+class ProofingEvents
+{
+    public const IMAGE_UPDATED = 'aperture_pro/proofing.image_updated';
+    public const SUBMITTED     = 'aperture_pro/proofing.submitted';
+    public const REOPENED      = 'aperture_pro/proofing.reopened';
+    public const COMPLETED     = 'aperture_pro/proofing.completed';
+}
